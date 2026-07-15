d12cc6b935ecd442a7cec7131fb6f4e8c6d4cf61
Seemann <mail@sannybuilder.com>
2026-07-14 22:37:50 -0400

    defensive TxdManager

--- newOpcodes/TxdManager.cpp
+++ newOpcodes/TxdManager.cpp
@@ -27,7 +27,12 @@ bool TxdManager::AddTxd(char *name, char *path)
 		freeTxd->m_isUsed = true;
 		strcpy(freeTxd->m_name, name);
 		freeTxd->m_id = CTxdStore::AddTxdSlot(name);
-		CTxdStore::LoadTxd(freeTxd->m_id, path);
+		if (!CTxdStore::LoadTxd(freeTxd->m_id, path))
+		{
+			CTxdStore::RemoveTxdSlot(freeTxd->m_id);
+			freeTxd->m_id = -1;
+			return false;
+		}
 		CTxdStore::AddRef(freeTxd->m_id);
 		return true;
 	}
@@ -46,11 +51,14 @@ void TxdManager::CleanAll()
 				TxdDef *txd = CTxdStore::ms_pTxdPool->GetAt(m_aTxds[i].m_id);
 				if(txd)
 				{
-					// Balances the AddRef call from AddTxd.
+					// Mirrors the calls in TxdManager::AddTxd
+
 					CTxdStore::RemoveRef(m_aTxds[i].m_id);
-					txd = CTxdStore::ms_pTxdPool->GetAt(m_aTxds[i].m_id);
-					if(txd)
+					if(txd->m_wRefsCount <= 0)
+					{
+						CTxdStore::RemoveTxd(m_aTxds[i].m_id);
 						CTxdStore::RemoveTxdSlot(m_aTxds[i].m_id);
+					}
 				}
 			}
 			m_aTxds[i].m_isUsed = false;
